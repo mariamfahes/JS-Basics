7d335645d059543f781e894cf75200c4fae0d7d6
mariamfahes <fahesmariam20@gmail.com>
2020-09-28 16:21:09 +0300

    change step 5

--- javascript-basics-02/step 5/Step_5.js
+++ javascript-basics-02/step 5/Step_5.js
@@ -6,11 +6,3 @@ img.addEventListener("mouseover",function(){
     document.getElementById("image4").src="images/image4_2.jpg";
     document.getElementById("image5").src="images/image5_2.jpg";
 });
-
-img.addEventListener("mouseout",function(){
-    img.src="images/image1.jpg";
-    document.getElementById("image2").src="images/image2.jpg";
-    document.getElementById("image3").src="images/image3.jpg";
-    document.getElementById("image4").src="images/image4.jpg";
-    document.getElementById("image5").src="images/image5.jpg";
-});
---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,20 @@
+   
+  var img=document.getElementById("image1");
+  img.addEventListener("mouseover",function(){
+      img.src="images/image1_2.jpg";
+      document.getElementById("image2").src="images/image2_2.jpg";
+      document.getElementById("image3").src="images/image3_2.jpg";
+      document.getElementById("image4").src="images/image4_2.jpg";
+      document.getElementById("image5").src="images/image5_2.jpg";
+  }); 
+   
+   
+   
+   
+   img.addEventListener("mouseout",function(){
+    img.src="images/image1.jpg";
+    document.getElementById("image2").src="images/image2.jpg";
+    document.getElementById("image3").src="images/image3.jpg";
+    document.getElementById("image4").src="images/image4.jpg";
+    document.getElementById("image5").src="images/image5.jpg";
+});
