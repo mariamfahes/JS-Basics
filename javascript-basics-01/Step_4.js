d7b1c416f4fdd64ba9c7feffd97f03ca4927dc3a
mariamfahes <fahesmariam20@gmail.com>
2020-09-28 12:05:57 +0300

    step5

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+let name = window.prompt("enter your name");
+let surname = window.prompt("enter your surname");
+let city = window.prompt("enter your city");
+
+alert( "hello"+name+" \n "+surname + "\n"+ city);
---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,3 @@
+let n1= window.prompt("enter your n1");
+let n2= window.prompt("enter your n2");
+alert(n1*n2);
